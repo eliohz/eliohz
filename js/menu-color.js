2d8c22a6aa8fa340ecdcd8adfd8def075cb16898
eliohz <70689140+eliohz@users.noreply.github.com>
2022-06-17 11:36:57 +0200

    Update menu-color.js

--- js/menu-color.js
+++ js/menu-color.js
@@ -14,12 +14,12 @@
 
 $(function(){
     $(window).scroll(function(){
-        if($(this).scrollTop() > 2000) {
+        if($(this).scrollTop() > 100) {
             $('#topbar, .cart-label').fadeOut('slow');
             $('.logo img')
                 .attr('src','img/logo.png');
         }
-        if($(this).scrollTop() < 2000) {
+        if($(this).scrollTop() < 100) {
             $('#logo, #topbar, .cart-label').fadeIn('fast');
             $('.logo img')
                 .attr('src','img/logo-light.png');
